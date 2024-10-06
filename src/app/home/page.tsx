49127d97ef989b231a255ed27ba982e8019f1c9d
Manuel Marcía <manuel.marcia@shopstack.io>
2024-10-06 15:06:19 -0600

    fix: avoid deploy errors

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,13 +1,30 @@
 "use client";
-import { useSession } from "next-auth/react";
+import { useEffect } from 'react';
+import { useSession } from "next-auth/react"; 
+import { useRouter } from 'next/navigation';
 import AdminUserManagement from "@/components/AdminUserManagement";
 import ProjectManagerHome from "@/components/ProjectManagerHome";
 import TeamMemberHome from "@/components/TeamMemberHome";
 
 function HomePage() {
   const { data: session, status } = useSession();
+  const router = useRouter();
 
   if (status === "loading") return <div>Loading...</div>;
+  
+  useEffect(() => {
+    //Si el estado de autenticación es "authenticated", verifica el rol del usuario
+    if (status === "authenticated") {
+      const userRole = session?.user?.role;
+
+      if (userRole !== "Admin" && userRole !== "ProjectManager") {
+        router.push("/access-denied");  //Redirige si no es Admin o Project Manager
+      }
+    } else if (status === "unauthenticated") {
+      router.push("/login");  //Redirige al login si no está autenticado
+    }
+  }, [status, session, router]);
+
   if (!session || !session.user) {
     return <div>Access Denied</div>;
   }
