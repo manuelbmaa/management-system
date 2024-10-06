d5c15754e2bf3598a55813411054a6646a6e5ef7
fjose4210 <132023217+fjose4210@users.noreply.github.com>
2024-10-05 21:28:56 -0600

    CambioBueno
    JoseFigueroa

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -4,6 +4,7 @@ import { Pie, Bar } from 'react-chartjs-2';
 import { Chart as ChartJS, ArcElement, Tooltip, Legend, CategoryScale, LinearScale, BarElement } from 'chart.js';
 import { useState, useEffect } from 'react';
 import { Project } from "../../components/ProjectManagerHome"; 
+import { Member } from "../../components/ProjectManagerHome"; 
 
 ChartJS.register(ArcElement, Tooltip, Legend, CategoryScale, LinearScale, BarElement);
 
@@ -12,26 +13,39 @@ function DashboardPage() {
   const [taskTotalCount, setTaskTotalCount] = useState(0); //Estado para el total de tareas
   const [projectCount, setProjectCount] = useState(0); //Estado para el número total de proyectos
   const [projects, setProjects] = useState<Project[]>([]); //Estado para la lista de proyectos
+  
+  const [roleCount, setRoleCount] = useState({
+    Admin: 0,
+    ProjectManager: 0,
+    TeamMember: 0,
+  });
 
-  //Estado para las tareas agregadas en el gráfico de barras
+  //Estado para las tareas agregadas en el gráfico de pastel
   const [taskStats, setTaskStats] = useState({
     realizadas: 0,
     pendientes: 0,
     finalizadas: 0,
   });
 
-  //Estado para el gráfico de pastel 
-  const [projectStatusCount, setProjectStatusCount] = useState({
-    completados: 0,
-    enProgreso: 0,
-  });
-
-  //Función para obtener el número de usuarios
+  //Función para obtener el número de usuarios y sus roles
   const fetchUserCount = async () => {
     try {
       const response = await fetch('/api/users'); 
-      const data = await response.json();
+      const data: { users: Member[] } = await response.json(); //Asigna el tipo 'Member[]' a los usuarios
+
       setUserCount(data.users.length); //Asigna el número de usuarios
+
+      //Contar roles
+      const adminCount = data.users.filter((user: Member) => user.role === 'Admin').length;
+      const projectManagerCount = data.users.filter((user: Member) => user.role === 'ProjectManager').length;
+      const teamMemberCount = data.users.filter((user: Member) => user.role === 'TeamMember').length;
+
+      setRoleCount({
+        Admin: adminCount,
+        ProjectManager: projectManagerCount,
+        TeamMember: teamMemberCount,
+      });
+
     } catch (error) {
       console.error('Error fetching user count:', error);
     }
@@ -43,15 +57,10 @@ function DashboardPage() {
       const response = await fetch("/api/projects");
       const data = await response.json();
       if (data) {
-        setProjects(data); //Establece la lista de proyectos
-        setProjectCount(data.length); //Establece el número total de proyectos
+        setProjects(data); //Se establece la lista de proyectos
+        setProjectCount(data.length); //Se establece el número total de proyectos
 
-        //Actualiza el conteo de estados de proyectos para el gráfico de pastel
-        const completados = data.filter((project: Project) => project.status === 'Completado').length;
-        const enProgreso = data.filter((project: Project) => project.status === 'En progreso').length;
-        setProjectStatusCount({ completados, enProgreso });
-
-        //Calcula estadísticas de tareas y total de tareas
+        //Se calcula estadísticas de tareas y total de tareas
         let realizadas = 0;
         let pendientes = 0;
         let finalizadas = 0;
@@ -106,19 +115,7 @@ function DashboardPage() {
     },
   };
 
-  //Datos dinámicos para el gráfico de pastel de proyectos
-  const projectoCompleto = {
-    labels: ['Completados', 'En Progreso'],
-    datasets: [
-      {
-        label: 'Proyectos',
-        data: [projectStatusCount.completados, projectStatusCount.enProgreso], //Datos calculados dinámicamente
-        backgroundColor: ['#36A2EB', '#FF6384'],
-      },
-    ],
-  };
-
-  //Datos dinámicos para el gráfico de barras de tareas
+  //Datos dinámicos para el gráfico de pastel de tareas
   const tareas = {
     labels: ['Realizadas', 'Pendientes', 'En Progreso'],
     datasets: [
@@ -148,18 +145,20 @@ function DashboardPage() {
           <h1 className="text-4xl font-bold text-black text-center">Dashboard</h1>
         </div>
 
-        {/*Tarjetas*/} 
+        
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 w-full max-w-6xl mb-8">
           <div className="bg-white p-6 rounded-lg shadow-lg hover:shadow-xl border border-black transition">
-            <h3 className="text-xl font-semibold text-black mb-4">Total de Tareas</h3>
-            <p className="text-3xl font-bold text-black">{taskTotalCount}</p> {/* Mostrar el número total de tareas */}
+            <h3 className="text-xl font-semibold text-black mb-4 text-center">Total de Tareas</h3>
+            <p className="text-3xl font-bold text-black">{taskTotalCount}</p> {/*Muestra el número total de tareas*/}
           </div>
           <div className="bg-white p-6 rounded-lg shadow-lg hover:shadow-xl border border-black transition">
-            <h3 className="text-xl font-semibold text-black mb-4">Ganado</h3>
-            <p className="text-3xl font-bold text-black">$12,345</p>
+            <h3 className="text-xl font-semibold text-black mb-4 text-center">Roles de Usuarios</h3>
+            <p className="text-lg font-bold text-black">Admin: {roleCount.Admin}</p>
+            <p className="text-lg font-bold text-black">Project Manager: {roleCount.ProjectManager}</p>
+            <p className="text-lg font-bold text-black">Team Member: {roleCount.TeamMember}</p>
           </div>
           <div className="bg-white p-6 rounded-lg shadow-lg hover:shadow-xl border border-black transition">
-            <h3 className="text-xl font-semibold text-black mb-4">Usuarios</h3>
+            <h3 className="text-xl font-semibold text-black mb-4 text-center">Usuarios</h3>
             <p className="text-3xl font-bold text-black">{userCount}</p> {/*Muestra el número de usuarios*/}
           </div>
         </div>
@@ -168,17 +167,11 @@ function DashboardPage() {
         <div className="bg-white p-6 rounded-lg shadow-lg border border-black w-full max-w-6xl">
           <h3 className="text-xl font-semibold text-black mb-4 text-center">Vista de análisis</h3>
 
-          <div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
-            {/*Gráfico de pastel*/}
+          <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
+            {/*Gráfico de pastel de tareas*/}
             <div className="bg-white p-4 rounded-lg shadow-lg border border-black">
-              <h4 className="text-center text-lg font-semibold text-black mb-2">Proyectos Completados vs En Progreso</h4>
-              <Pie data={projectoCompleto} />
-            </div>
-
-            {/*Gráfico de barras*/}
-            <div className="bg-white p-4 rounded-lg shadow-lg border border-black">
-              <h4 className="text-center text-lg font-semibold text-black mb-2">Tareas</h4>
-              <Bar data={tareas} options={options} />
+              <h4 className="text-center text-lg font-semibold text-black mb-2">Estadísticas de Tareas</h4>
+              <Pie data={tareas} />
             </div>
 
             {/*Gráfico de proyectos totales*/}
