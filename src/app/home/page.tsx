78481f23428f3633ca69d600e3bb2b862c73808c
Manuel Marcía <manuel.marcia@shopstack.io>
2024-10-06 15:10:48 -0600

    fix: avoid error react hook useeffect

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,7 +1,7 @@
 "use client";
-import { useEffect } from 'react';
+import { useEffect } from "react";
 import { useSession } from "next-auth/react"; 
-import { useRouter } from 'next/navigation';
+import { useRouter } from "next/navigation";
 import AdminUserManagement from "@/components/AdminUserManagement";
 import ProjectManagerHome from "@/components/ProjectManagerHome";
 import TeamMemberHome from "@/components/TeamMemberHome";
@@ -10,35 +10,32 @@ function HomePage() {
   const { data: session, status } = useSession();
   const router = useRouter();
 
-  if (status === "loading") return <div>Loading...</div>;
-  
   useEffect(() => {
-    //Si el estado de autenticación es "authenticated", verifica el rol del usuario
+    if (status === "loading") return; // Evita hacer algo mientras se carga
+
+    // Si el estado de autenticación es "authenticated", verifica el rol del usuario
     if (status === "authenticated") {
       const userRole = session?.user?.role;
 
       if (userRole !== "Admin" && userRole !== "ProjectManager") {
-        return router.push("/access-denied");  //Redirige si no es Admin o Project Manager
+        router.push("/access-denied");  // Redirige si no es Admin o Project Manager
       }
     } else if (status === "unauthenticated") {
-      return router.push("/login");  //Redirige al login si no está autenticado
+      router.push("/login");  // Redirige al login si no está autenticado
     }
   }, [status, session, router]);
+
+  // Comprobación de acceso antes de renderizar el contenido
+  if (status === "loading" || !session || !session.user) {
+    return <div>Loading...</div>; // Muestra un mensaje de carga o acceso denegado
+  }
 
   return (
     <div className="dark:text-white max-w-4xl mx-auto p-4">
       <h1 className="text-2xl font-bold mb-4">Home</h1>
-      {session?.user.role === "Admin" && (
-        <>
-          <AdminUserManagement />
-        </>
-      )}
-      {session?.user.role === "ProjectManager" && (
-        <ProjectManagerHome />
-      )}
-      {session?.user.role === "TeamMember" && (
-        <TeamMemberHome />
-      )}
+      {session.user.role === "Admin" && <AdminUserManagement />}
+      {session.user.role === "ProjectManager" && <ProjectManagerHome />}
+      {session.user.role === "TeamMember" && <TeamMemberHome />}
     </div>
   );
 }
